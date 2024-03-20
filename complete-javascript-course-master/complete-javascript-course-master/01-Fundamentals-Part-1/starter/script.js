6a2369f0bee9bbd24834bb697af40067305e9398
Unknownmaster0 <sagar192002singh@gmail.com>
2024-03-21 00:28:43 +0530

    learning the javascript from udemy

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let val = 5;
+if(val === 5) alert("javascript is fun!");
+// using console.log() -> you can print anything onto the console of the browser.
+console.log(4+5+5);
+console.log("This is sagar singh learning javascript.");
